0928be7ec82b0e43ce86dd888bfdfdccd0addfc4
iimr4n <iimr3n@gmail.com>
2026-01-29 14:18:51 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Hero";
+import About from "../components/About";
 import Contact from "../components/Contact";
 
 export default function Page() {
   return (
     <>
       <Hero />
+      <About />
       <Contact />
     </>
   );

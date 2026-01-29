90e04cf3e413a648a258db13ca4ecd22721d83f9
iimr4n <iimr3n@gmail.com>
2026-01-29 19:00:03 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,15 +3,16 @@ import About from "../components/About";
 import Services from "../components/Services";
 import Projects from "../components/Projects";
 import Footer from "../components/Footer";
+import RevealProvider from "../components/RevealProvider";
 
 export default function Home() {
   return (
-    <>
+    <RevealProvider>
       <Hero />
       <About />
       <Services />
       <Projects />
       <Footer />
-    </>
+    </RevealProvider>
   );
 }

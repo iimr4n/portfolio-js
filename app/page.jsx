b27d001e053c260ce515a73204791526253c2dc6
iimr4n <iimr3n@gmail.com>
2026-01-29 13:40:09 +0530

    Add Page component with Hero and Contact sections

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero";
+import Contact from "../components/Contact";
+
+export default function Page() {
+  return (
+    <>
+      <Hero />
+      <Contact />
+    </>
+  );
+}

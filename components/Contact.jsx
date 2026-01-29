91dfd0a0324679310bce0ab8ed1233663a7d33a6
iimr4n <iimr3n@gmail.com>
2026-01-29 13:51:41 +0530

    Enhance Contact component with additional details

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,7 +1,14 @@
 export default function Contact() {
   return (
-    <section>
-      <p>Contact: iimr3n@gmail.com</p>
+    <section id="contact">
+      <div className="container">
+        <h3>Let’s Connect</h3>
+        <p>
+          📧 <a href="mailto:iimr3n@gmail.com">iimr3n@gmail.com</a>
+          <br />
+          📞 +91 9061098676
+        </p>
+      </div>
     </section>
   );
 }

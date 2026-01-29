5700898b05bdfd6e1b9525be2d66f648e0784555
iimr4n <iimr3n@gmail.com>
2026-01-29 14:38:44 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -37,6 +37,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+  <meta
+    name="google-site-verification"
+    content="<meta name="google-site-verification" content="S9DCP7jd0PaVAlfICqrOBHJ6QqxjlQJgigBZpkNJntI" />"
+  />
+</head>
       <body>{children}</body>
     </html>
   );

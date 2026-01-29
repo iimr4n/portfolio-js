bf49b1e1b2d1fa6d69a7bb8084deae0433074591
iimr4n <iimr3n@gmail.com>
2026-01-29 14:05:43 +0530

    Add contact information and social links

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,11 +3,30 @@ export default function Contact() {
     <section id="contact">
       <div className="container">
         <h3>Let’s Connect</h3>
+
         <p>
           📧 <a href="mailto:iimr3n@gmail.com">iimr3n@gmail.com</a>
           <br />
           📞 +91 9061098676
         </p>
+
+        <div className="socials">
+          <a
+            href="https://www.linkedin.com/in/7imran"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+
+          <a
+            href="https://www.instagram.com/iimr3n"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+        </div>
       </div>
     </section>
   );

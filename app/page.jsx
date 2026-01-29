2b31db770758b17a5f17ba9deb68a663f374e407
iimr4n <iimr3n@gmail.com>
2026-01-29 14:47:44 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import Hero from "../components/Hero";
 import About from "../components/About";
+import Skills from "../components/Skills";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
@@ -8,6 +9,7 @@ export default function Page() {
     <>
       <Hero />
       <About />
+      <Skills />
       <Contact />
       <Footer />
     </>

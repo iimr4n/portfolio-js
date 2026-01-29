5bc28c26596678ed7500cc8e52d9d70014ffbde8
iimr4n <iimr3n@gmail.com>
2026-01-29 14:26:59 +0530

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>
+          © {new Date().getFullYear()} AHAMMAD IMRAN A. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
